02164ce71eda8bc1317c20cf61182f8a6779e42c
baipengyan <1762479428@qq.com>
2019-12-03 11:57:49 +0800

    hello

---
+++ src/com/bai/Main.java
@@ -0,0 +1,16 @@
+package com.bai;
+
+/**
+ * @author baipengyan
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("你是个傻逼");
+        System.out.println("1" + "2");
+        System.out.println("hah");
+        System.out.println("hl;l");
+        System.out.println("hhas");
+    }
+}
